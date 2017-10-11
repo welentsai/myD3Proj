add73f904724138ab1cd094bdeb9d06d3dc51702
welen.tsai <welentsai@gmail.com>
2017-10-11 21:30:50 +0800

    add up node moudule - pandas.js babel-runtime

---
+++ pandas_test.js
@@ -0,0 +1,27 @@
+
+
+const Series = require('pandas-js').Series;
+const DataFrame = require('pandas-js').DataFrame;
+
+const mgMgr = require('./lib/dataFormatter/mgMgr');
+
+// let symbls = ['FedRate', 'exRate', 'SP500', 'TW50'];
+let symbls = ['FedRate'];
+let _startDate = '2017-01-01';
+let _endDate = '2017-10-31';
+
+
+mgMgr.getData(symbls, _startDate, _endDate, function (err, rows) {
+  if(err) {
+    console.log(err);
+  } else {
+    let _res = {
+      status: 'ok',
+      data: rows
+    };
+
+    console.log(_res);
+    const ds_1 = new Series([1, 2, 3, 4], {name: 'My Data 1'});
+    console.log(ds_1.toString());
+  }
+});
