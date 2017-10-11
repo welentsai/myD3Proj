c453961719f97668d681546e3c1b010e8f87f7bb
welen.tsai <welentsai@gmail.com>
2017-10-11 23:17:44 +0800

    add up pandasJS test code

--- pandas_test.js
+++ pandas_test.js
@@ -1,12 +1,14 @@
 
+const moment = require('moment');
 
 const Series = require('pandas-js').Series;
 const DataFrame = require('pandas-js').DataFrame;
 
 const mgMgr = require('./lib/dataFormatter/mgMgr');
 
-// let symbls = ['FedRate', 'exRate', 'SP500', 'TW50'];
-let symbls = ['FedRate'];
+let symbls = ['FedRate', 'exRate', 'SP500', 'TW50'];
+// let symbls = ['FedRate'];
+//let symbls = ['exRate'];
 let _startDate = '2017-01-01';
 let _endDate = '2017-10-31';
 
@@ -15,14 +17,24 @@ mgMgr.getData(symbls, _startDate, _endDate, function (err, rows) {
   if(err) {
     console.log(err);
   } else {
-    // let _res = {
-    //   status: 'ok',
-    //   data: rows
-    // };
 
-    // console.log(rows[0].fedRate);
-    // const ds_1 = new Series([1, 2, 3, 4], {name: 'My Data 1'});
-    const df = new DataFrame(rows[0].fedRate);
-    console.log(df.toString());
+  	for(let i = 0; i < rows.length; i++) {
+  		console.log(rows[i].tag);
+  	}
+
+    // if(rows[0].fedRate.length > 0 ) {
+    // 	let rowList = [];
+    	
+    // 	for(let i = 0; i < rows[0].fedRate.length; i++) {
+    // 		let row = {
+    // 			date: moment(rows[0].fedRate[i].date).format('YYYY-MM-DD'),
+    // 			rate: rows[0].fedRate[i].rate
+    // 		};	
+    // 		console.log(i);
+    // 		rowList.push(row);
+    // 	}
+    // 	const df = new DataFrame(rowList);
+    // 	console.log(df.toString());
+    // }
   }
 });
